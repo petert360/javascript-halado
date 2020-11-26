834f01e7f48e1a3190a71ba8ce8892211f2560c2
petert360 <petert.360@hotmail.com>
2020-11-26 13:33:56 +0100

    Első feladat megoldása

--- 05-szamok/js/feladat1.js
+++ 05-szamok/js/feladat1.js
@@ -10,7 +10,20 @@
 
 // 2. lépés miután megvannak a paraméterek egy tömbben, a tömb elemein végigmegyek és összeadom őket.
 // ez megoldható for ciklussal ez lesz a sumParameters1
-// de megoldható 
+// ugyanakkor megoldható a .reduce() tömb metódussal is ez lesz a sumParameters2 függvény.
+/*
+Az Array.reduce() metódus
+forrás: 
+https://www.javascripttutorial.net/javascript-array-reduce/
+https://javascript.info/array-methods#reduce-reduceright
+
+szintaxis: array.reduce(reducer [, initialValue]);
+A reducer() egy függvény hívás (callback function), amit minden egyes elemre meghív a metódus.
+A reducer() visszatérési értéke akkumulálódik és argumentumként adódik át a következő reducer() híváskor.
+Ha nem adok meg initialValue-t, akkor a tömb első elemét veszi annak és a második elemtől kezdődik az iteráció. Ez azért igényel odafigyelést, mert ha a tömb üres, hibát eredményez a metódus hívás.
+
+Harmadik lépésben arrFn-t hozok létre a fnDec helyett.
+*/
 
 function sumParameters1(...param) {
     let sum = 0;
@@ -21,4 +34,37 @@ function sumParameters1(...param) {
     console.log(sum);
 }
 
+//isSafeInteger figyelés if..else megoldással
+function sumParameters2(...param) {
+    let sum = param.reduce(function reducer(accumulator, current) {
+        if (Number.isSafeInteger(accumulator) && Number.isSafeInteger(current)) {
+            return accumulator + current;
+        } else {
+            return BigInt(accumulator) + BigInt(current);
+        }
+    })
+    console.log(sum);
+}
+
+function sumParameters3(...param) {
+    console.log(param.reduce((accumulator, current) => (accumulator + current)))
+}
+
+//2-es megoldás arrFn-nél
+function sumParameters4(...param) {
+    console.log(param.reduce((accumulator, current) => {
+        if (Number.isSafeInteger(accumulator) && Number.isSafeInteger(current)) {
+            return accumulator + current;
+        } else {
+            return BigInt(accumulator) + BigInt(current);
+        }
+
+        }))
+}
+
+
+
 sumParameters1(1, 2, 3);
+sumParameters2(1, Number.MAX_VALUE, 3);
+sumParameters3(1, 2, 3);
+sumParameters4(1, Number.MAX_VALUE, 3);
