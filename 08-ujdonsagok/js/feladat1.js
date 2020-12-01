26fa081fd8ee7a5951c00c2d75dae9aae5e5bfd6
petert360 <peter.t360@hotmail.com>
2020-12-01 22:05:54 +0100

    Feladat 1 megoldása, feladat 2 előkészítése

--- 08-ujdonsagok/js/feladat1.js
+++ 08-ujdonsagok/js/feladat1.js
@@ -1,2 +1,29 @@
 //1. Írj egy függvény kifejezést, ami a paraméterként megadott tömbből eltávolítja a duplikált elemeket! A függvény visszatérési értéke a duplikált elemektől mentes új tömb!
 
+
+const sampleArr = [4, 5, 7, 8, 7, 9];
+
+function deleteDuplicates(arr) {
+    let newSet = new Set();
+    for (let i = 0; i < arr.length; i += 1) {
+        newSet.add(arr[i]);
+    }
+    return Array.from(newSet);
+}
+
+console.log(deleteDuplicates(sampleArr));
+
+
+
+//https://www.samanthaming.com/tidbits/43-3-ways-to-remove-array-duplicates/
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+// Use the spread operator to transform a set into an Array.
+// console.log([...mySet])
+
+function deleteDuplicates2(arr) {
+    const newSet = new Set(arr);
+    return [...newSet];
+}
+
+console.log(deleteDuplicates2(sampleArr));
---
+++ 08-ujdonsagok/js/feladat2.js
@@ -0,0 +1,13 @@
+/*
+2. Feladat
+Készíts egy `HU` nevű objektumot, aminek az alábbi propertijei vannak:
+- `date()`: Visszaadja a paraméterként megadott dátumot a magyar dátumírás szabályainak megfelelően
+- `currency()`: Visszaadja a paraméterként megadott számot a magyar pénzformátumnak megfelelően a Ft végződéssel együtt
+- `list()`: A paraméterként kapott string tömböt visszaadja az alábbi formában: első, második és harmadik" (Tehát vesszőkkel elválasztva az utolsó elem előtt az és szóval)
+*/
+
+const hu = {
+
+
+
+}
